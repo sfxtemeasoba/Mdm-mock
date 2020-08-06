8cb88c1f1db1719d4730a0d98f8f907045f5db19
Emeasoba Tochukwu <temeasoba@seamfix.com>
2020-08-06 09:45:46 +0100

    Initialize mdm mock project

---
+++ src/main/java/com/sf/mdmmock/mdm/MdmApplication.java
@@ -0,0 +1,13 @@
+package com.sf.mdmmock.mdm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sf/mdmmock/mdm/MdmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sf.mdmmock.mdm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
